88c1f3af2dd2fc0b3dc50f411d9359a8798389d5
Hadi-SayedAhmad <121687873+Hadi-SayedAhmad@users.noreply.github.com>
2023-10-04 18:58:39 +0300

    create user controllers and routes

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
 import express from "express";
+import dotenv from "dotenv"
+import connectDB from "./config/db.js"
 const app = express();
 const port = 3000;
-import dotenv from "dotenv"
 dotenv.config();
-import connectDB from "./config/db.js"
+import userRouter from "./routes/userRoutes.js"
 connectDB();
+
 app.get("/", (req, res) => {
     res.send("Api is running...");
 })
+
+app.use("/api/user", userRouter);
+
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
