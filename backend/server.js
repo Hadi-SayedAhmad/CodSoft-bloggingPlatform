2a2a9c4b180b95bdbe7946c46326dc5e6f3259fd
Hadi-SayedAhmad <121687873+Hadi-SayedAhmad@users.noreply.github.com>
2023-10-04 20:14:56 +0300

    sign up controller and bcrypt

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,11 @@ import express from "express";
 import dotenv from "dotenv"
 import connectDB from "./config/db.js"
 const app = express();
+app.use(express.json())
 const port = 3000;
 dotenv.config();
 import userRouter from "./routes/userRoutes.js"
+import authRouter from "./routes/authRoutes.js"
 connectDB();
 
 app.get("/", (req, res) => {
@@ -12,7 +14,7 @@ app.get("/", (req, res) => {
 })
 
 app.use("/api/user", userRouter);
-
+app.use("/api/auth", authRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
