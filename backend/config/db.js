e681d4d7d28615358aab6b85af88fb731b11a5b8
Hadi-SayedAhmad <121687873+Hadi-SayedAhmad@users.noreply.github.com>
2023-10-04 18:23:00 +0300

    Connect MongoDB

---
+++ backend/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+async function connectDB() {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Connected with MongoDB successfully!");
+    } catch (error) {
+        console.log(`MONGO DB CONNECTION ERROR: ${error.message}`);
+        process.exit(1);
+    }
+}
+
+export default connectDB
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 const app = express();
 const port = 3000;
-
+import dotenv from "dotenv"
+dotenv.config();
+import connectDB from "./config/db.js"
+connectDB();
 app.get("/", (req, res) => {
     res.send("Api is running...");
 })
