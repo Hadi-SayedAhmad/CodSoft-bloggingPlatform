60963cc8b05d515c7df290b89410d6dc9161c276
Hadi-SayedAhmad <121687873+Hadi-SayedAhmad@users.noreply.github.com>
2023-10-05 14:49:10 +0300

    sign in controller and route (jwt)

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,5 +1,6 @@
 import asyncHandler from "../middlewares/asyncHandler.js";
 import User from "../models/userModel.js"
+import {generateToken} from "../utils/generateToken.js"
 
 export const signUp = asyncHandler(async (req, res) => {
     const { username, email, password } = req.body;
@@ -9,4 +10,21 @@ export const signUp = asyncHandler(async (req, res) => {
         password
     })
     res.status(201).json("User created successfully!");
+})
+
+
+export const signIn = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({email});
+    if (user && (await user.matchPassword(password))) {
+        generateToken(user._id, res);
+        res.status(200).json({
+            _id: user._id,
+            name: user.username,
+            email: user.email,
+        });
+    } else {
+        res.status(404);
+        throw new Error("Invalid email or passowrd!");
+    }
 })
