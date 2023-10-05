39f0d9db1c41aee7d699ba30d5d5e488214cd7c9
Hadi-SayedAhmad <121687873+Hadi-SayedAhmad@users.noreply.github.com>
2023-10-05 17:18:01 +0300

    Add Redux Toolkit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,20 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import "./index.css"
-import {Route, createBrowserRouter, RouterProvider, createRoutesFromElements} from "react-router-dom"
+import { Route, createBrowserRouter, RouterProvider, createRoutesFromElements } from "react-router-dom"
 import Home from './pages/Home.jsx'
 import Profile from './pages/Profile.jsx'
 import SignIn from './pages/SignIn.jsx'
 import SignUp from './pages/SignUp.jsx'
-
+import { store } from './store.js'
+import { Provider } from 'react-redux'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
-        <Route path='/' index={true} element={<Home />}></Route>
-        <Route path='/profile' element={<Profile />}></Route>
-        <Route path='/sign-in' element={<SignIn />}></Route>
-        <Route path='/sign-up' element={<SignUp />}></Route>
+      <Route path='/' index={true} element={<Home />}></Route>
+      <Route path='/profile' element={<Profile />}></Route>
+      <Route path='/sign-in' element={<SignIn />}></Route>
+      <Route path='/sign-up' element={<SignUp />}></Route>
 
 
     </Route>
@@ -24,7 +25,8 @@ const router = createBrowserRouter(
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
-    <RouterProvider router={router}></RouterProvider>
+    <Provider store={store}>
+      <RouterProvider router={router}></RouterProvider>
+    </Provider>
   </React.StrictMode>,
 )
